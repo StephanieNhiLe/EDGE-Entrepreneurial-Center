bff5ae2cc9e693cfc9aaae78a05ef3949b87827f
Stephanie Nhi Le <ledaquynhnhi@gmail.com>
2023-08-19 02:07:44 -0500

    add exec board section

--- about.php
+++ about.php
@@ -69,7 +69,64 @@
 <div class="section-container">
     <div class="content-container">
         <h2 class="title">DIRECTORY</h2>
-        
+        <h2 class="sub-title">Board of Directors</h2>
+        <div class="row">
+            <div class="col-md-3 profile text-center">
+                <div class="img-box">
+                    <img src="img/exec-board/person1.jpg" alt="exec-board" class="img-responsive">
+                    <ul>
+                        <a href="#"><li><i class="fa-solid fa-phone"></i></li></a>
+                        <a href="#"><li><i class="fa-solid fa-envelope"></i></li></a>
+                        <a href="#"><li><i class="fa-brands fa-linkedin"></i></li></a>
+                    </ul>
+                </div>
+                <h2>Kirk Doonan</h2>
+                <h3>President</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 profile text-center">
+                <div class="img-box">
+                    <img src="img/exec-board/person1.jpg" alt="exec-board" class="img-responsive">
+                    <ul>
+                        <a href="#"><li><i class="fa-solid fa-phone"></i></li></a>
+                        <a href="#"><li><i class="fa-solid fa-envelope"></i></li></a>
+                        <a href="#"><li><i class="fa-brands fa-linkedin"></i></li></a>
+                    </ul>
+                </div>
+                <h2>Kirk Doonan</h2>
+                <h3>President</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 profile text-center">
+                <div class="img-box">
+                    <img src="img/exec-board/person1.jpg" alt="exec-board" class="img-responsive">
+                    <ul>
+                        <a href="#"><li><i class="fa-solid fa-phone"></i></li></a>
+                        <a href="#"><li><i class="fa-solid fa-envelope"></i></li></a>
+                        <a href="#"><li><i class="fa-brands fa-linkedin"></i></li></a>
+                    </ul>
+                </div>
+                <h2>Kirk Doonan</h2>
+                <h3>President</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 profile text-center">
+                <div class="img-box">
+                    <img src="img/exec-board/person1.jpg" alt="exec-board" class="img-responsive">
+                    <ul>
+                        <a href="#"><li><i class="fa-solid fa-phone"></i></li></a>
+                        <a href="#"><li><i class="fa-solid fa-envelope"></i></li></a>
+                        <a href="#"><li><i class="fa-brands fa-linkedin"></i></li></a>
+                    </ul>
+                </div>
+                <h2>Kirk Doonan</h2>
+                <h3>President</h3>
+            </div>
+        </div>
+        <h2 class="sub-title">Staff</h2>
     </div>
 </div>
 <script>
